d3c3e95f2988499b505de80f0d637a995c0c6f5e
taketdev <maxleyh17@proton.me>
2026-05-10 05:26:29 +0200

    fixed menu error

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,5 +1,6 @@
-import { Component, inject, effect } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { ScrollService } from '../services/scroll.service';
 
 @Component({
@@ -11,8 +12,32 @@ import { ScrollService } from '../services/scroll.service';
 })
 export class Menu {
   public scrollService = inject(ScrollService);
+  private router = inject(Router);
 
   closeMenu() {
     this.scrollService.isMenuOpen.set(false);
+  }
+
+  scrollToSection(index: number) {
+    this.closeMenu();
+    const main = document.querySelector('main');
+    if (main) {
+      main.scrollTo({
+        top: index * main.clientHeight,
+        behavior: 'smooth'
+      });
+    } else {
+      this.router.navigateByUrl('/').then(() => {
+        requestAnimationFrame(() => {
+          const el = document.querySelector('main');
+          if (el) {
+            el.scrollTo({
+              top: index * el.clientHeight,
+              behavior: 'smooth'
+            });
+          }
+        });
+      });
+    }
   }
 }
